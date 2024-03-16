9a27e1c17d30eb4980aaaeff3f1d81df5303ab68
aswijapasupuleti <109501892+aswijapasupuleti@users.noreply.github.com>
2024-03-16 19:21:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+{
+    var conversion=require('./mertric');
+    x=10;
+    console.log('convert given number to centimeter',conversion.cm(x));
+    console.log('convert given number to kilometer',conversion.km(x));
+    console.log('convert given number to millimeter',conversion.mm(x));
+}
---
+++ metrics.js
@@ -0,0 +1,9 @@
+exports.cm=function(x){
+    return 100*x;
+}
+exports.km=function(x){
+    return 1000*x;
+}
+exports.mm=function(x){
+    return x/10;
+}
